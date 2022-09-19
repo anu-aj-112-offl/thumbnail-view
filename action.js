c54e0970d7b697a9981138e31e66b09492dc7ec3
anu-aj-112-offl <109644352+anu-aj-112-offl@users.noreply.github.com>
2022-09-19 11:59:05 +0530

    (feat): code updated

---
+++ action.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var image = $(".slide img");
+  
+    image.each(function (i) {
+      var container = $("nav ul");
+      var imageUrl = image[i].src;
+  
+      container.append(getimage(imageUrl));
+    });
+  
+    $("nav li img", this).click(function () {
+      var nav = $(this);
+      var url = nav.attr("src");
+  
+      image.attr("src", url);
+    });
+  
+    function getimage(imageUrl) {
+      return '<li><img src=" ' + imageUrl + ' " alt=""></li>';
+    }
+  });
+  
